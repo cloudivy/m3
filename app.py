50df7b6df02a1e709a095062d2cd529c45b3c24d
cloudivy <141609871+cloudivy@users.noreply.github.com>
2026-01-16 12:43:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.set_page_config(page_title="SCC Probability Visualization", layout="wide")
+st.set_page_config(page_title="Stress Corrosion Probability Visualization", layout="wide")
 
-st.title("🔬 SCC Probability Estimation - CHAKSU PANIPAT SECTION")
+st.title("🔬 Probability of Stress Corrosion - CHAKSU PANIPAT SECTION")
 
 uploaded_file = st.file_uploader("Choose scc_IV_dataset.xlsx", type=["xlsx"])
 
